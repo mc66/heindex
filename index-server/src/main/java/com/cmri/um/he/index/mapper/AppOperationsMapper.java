4fa18b9d6a38fd8f99c50d49350dd0bf04d3bc1b
machao <17600309619@163.com>
2018-06-13 17:05:08 +0800

    某类应用运营查询接口及测试类开发

---
+++ index-server/src/main/java/com/cmri/um/he/index/mapper/AppOperationsMapper.java
@@ -0,0 +1,45 @@
+package com.cmri.um.he.index.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 应用运营指数数据库访问的sql配置
+ *
+ * @author zhuyin
+ * Created on 2018/6/4
+ */
+@Mapper
+public interface AppOperationsMapper {
+
+    /**
+     * 查询指定月份期数、指定类别的应用数
+     *
+     * @param month    月份
+     * @param category 类别
+     * @return 应用数
+     */
+    @Select("SELECT COUNT(1) \n" +
+            "FROM app_operations app RIGHT JOIN app_category category ON app.category = category.id LEFT JOIN app_info active ON app.id = active.id  \n" +
+            "WHERE category.name = #{category} AND app.month=#{month}")
+    int countOfCategory(@Param("month") String month, @Param("category") String category);
+
+    /**
+     * 分页查询指定月份、指定类别的所有应用的运营数据
+     *
+     * @param month    月份
+     * @param category 类别
+     * @param offset   分页查询偏移量，从0开始
+     * @param rows     最多查询记录数
+     * @return 应用运营指数集合
+     */
+    @Select("SELECT app.name AS app, ope.content AS content, ope.channel AS channel,ope.tariff AS tariff,\n" +
+            "ope.service AS service,ope.market AS market,ope.experience AS experience\n" +
+            "FROM app_info app RIGHT JOIN app_category category ON app.category = category.id LEFT JOIN app_operations ope ON app.id = ope.app \n" +
+            "WHERE category.name = #{category} AND ope.month=#{month} ORDER BY ope.oindex DESC LIMIT #{offset}, #{rows}")
+    List<Map<String, Object>> getOperationsList(@Param("month") String month, @Param("category") String category, @Param("offset") int offset, @Param("rows") int rows);
+}
---
+++ index-server/src/main/java/com/cmri/um/he/index/service/impl/AppOperationsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.cmri.um.he.index.service.impl;
+
+import com.cmri.spring.common.data.PagingData;
+import com.cmri.um.he.index.dao.AppOperationsDao;
+import com.cmri.um.he.index.service.AppOperationsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * 查询当前应用类所有应用的运营数据的服务类的实现
+ *
+ * @author zhuyin
+ * Created on 2018/6/4
+ */
+@Service
+public class AppOperationsServiceImpl implements AppOperationsService {
+
+    @Autowired
+    AppOperationsDao appOperationsDao;
+
+    @Override
+    public PagingData<Map<String, Object>> getOperationsList(String month, String category, int page, int step) {
+        return new PagingData<>(appOperationsDao.countOfCategory(month, category),
+                page,
+                step,
+                appOperationsDao.getOperationsList(month, category, page, step)
+        );
+    }
+
+}
