5ab06e64e2107cd702d72614ac4c217f686fa884
machao <17600309619@163.com>
2018-06-14 15:13:48 +0800

    测试

--- index-server/src/main/java/com/cmri/um/he/index/controller/AppOperationsListController.java
+++ index-server/src/main/java/com/cmri/um/he/index/controller/AppOperationsListController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 体验指数，当前是根据期数查询当前应用类所有应用的运营数据
  *
@@ -23,9 +26,23 @@ public class AppOperationsListController extends ZRestController {
     private AppOperationsService appOperationsService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseMessage get(@RequestParam String month, String category, @RequestParam Integer page, @RequestParam Integer step) {
+    public ResponseMessage get(
+            @RequestParam String month,
+            String category,
+            @RequestParam Integer page,
+            @RequestParam Integer step,
+            HttpServletRequest request,
+            HttpServletResponse response) {
 
-            return appOperationsService.getOperationsList(month, category, page, step).updateResponse(genResponseMessage());
+        //以下三行response.set..是为了解决跨域访问拒绝的问题一定要写
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        // 响应类型
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        // 响应头设置
+        response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With");
+
+        return appOperationsService.getOperationsList(month, category, page, step).updateResponse(genResponseMessage());
+
 
     }
 }
