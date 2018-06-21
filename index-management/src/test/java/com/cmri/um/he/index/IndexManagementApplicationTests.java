7627a1f98adcc9f4382701667a3530a5041a14e0
lch <929664394@qq.com>
2018-06-21 11:01:33 +0800

    后台管理初始提交

---
+++ index-management/src/test/java/com/cmri/um/he/index/IndexManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cmri.um.he.index;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndexManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
