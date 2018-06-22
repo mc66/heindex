7d77fe02692061a87658940490c7da699bfa6464
yjj <yjj@zpark.com.cn>
2018-06-22 10:29:36 +0800

    创建求标准差的工具类

---
+++ index-management/src/main/java/com/cmri/um/he/index/util/StandardDeviationUtil.java
@@ -0,0 +1,64 @@
+package com.cmri.um.he.index.util;
+
+import java.text.DecimalFormat;
+/**
+ * 求取每一个维度的平均值和方差
+ * @author yjj
+ * Created on 2018/06/22
+ */
+public class StandardDeviationUtil {
+
+    /**
+     * 格式化Duble  取小数点后四位
+     */
+    private final static DecimalFormat df=new DecimalFormat("#,##0.0000");
+    /**
+     * 求取平均值
+     * @param x 维度数组
+     * @return 这个维度的平均值
+     */
+    public static double getAverage(double[] x){
+        int m = x.length;
+        int sum = 0;
+        for(int i = 0;i < m;i++){
+            sum += x[i];
+        }
+        return (double)(sum / m);
+    }
+
+    /**
+     * 求取标准差σ=sqrt(s^2)
+     * @param x 维度数组
+     * @return 这个维度的标准差
+     */
+    public static double getStandardDeviation(double[] x) {
+        int m=x.length;
+        double sum=0;
+        for(int i=0;i<m;i++){
+            sum+=x[i];
+        }
+        double dAve=sum/m;
+        double dVar=0;
+        for(int i=0;i<m;i++){
+            dVar+=(x[i]-dAve)*(x[i]-dAve);
+        }
+        double d= Math.sqrt(dVar/m);
+        String format = df.format(d);
+        Double aDouble = Double.valueOf(format);
+        return aDouble;
+    }
+
+    /**
+     * 求取评分和
+     * @param x 评分
+     * @return 这个维度的和
+     */
+    public static int getSum(int[] x){
+        int m = x.length;
+        int sum = 0;
+        for(int i = 0;i < m;i++){
+            sum += x[i];
+        }
+        return sum;
+    }
+}
