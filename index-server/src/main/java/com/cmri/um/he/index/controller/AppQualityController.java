f70d3a1a7465d0d9fbda47a0701e102a3466d15e
lim <2535780803@qq.com>
2018-07-04 09:52:04 +0800

    改接口

--- index-server/src/main/java/com/cmri/um/he/index/controller/AppQualityController.java
+++ index-server/src/main/java/com/cmri/um/he/index/controller/AppQualityController.java
@@ -31,14 +31,14 @@ public class AppQualityController extends ZRestController {
 
     /**
      *    查询功能指标
-     * @param app
+     * @param id
      * @param month
      * @return
      *  @author limin
      */
     @RequestMapping( value = "queryQualityFeatures", method = RequestMethod.GET)
-    public ResponseMessage queryQualityFeatures(@RequestParam Integer app,@RequestParam String month){
-        List resultList= appQualityService.queryQualityFeatures(app,month);
+    public ResponseMessage queryQualityFeatures(@RequestParam Integer id,@RequestParam String month){
+        List resultList= appQualityService.queryQualityFeatures(id,month);
         ResponseMessage responseMessage = this.genResponseMessage();
         responseMessage.set("features",resultList);
         return responseMessage;
@@ -46,14 +46,14 @@ public class AppQualityController extends ZRestController {
 
     /**
      *    查询界面指标
-     * @param app
+     * @param id
      * @param month
      * @return
      *  @author limin
      */
     @RequestMapping( value = "queryQualityViews", method = RequestMethod.GET)
-    public ResponseMessage queryQualityViews(@RequestParam Integer app,@RequestParam String month){
-        List resultList= appQualityService.queryQualityViews(app,month);
+    public ResponseMessage queryQualityViews(@RequestParam Integer id,@RequestParam String month){
+        List resultList= appQualityService.queryQualityViews(id,month);
         ResponseMessage responseMessage = this.genResponseMessage();
         responseMessage.set("views",resultList);
         return responseMessage;
@@ -61,14 +61,14 @@ public class AppQualityController extends ZRestController {
 
     /**
      *    查询时延指标
-     * @param app
+     * @param id
      * @param month
      * @return
      *  @author limin
      */
     @RequestMapping( value = "queryQualityDelay", method = RequestMethod.GET)
-    public ResponseMessage queryQualityDelay(@RequestParam Integer app,@RequestParam String month){
-        List resultList= appQualityService.queryQualityDelay(app,month);
+    public ResponseMessage queryQualityDelay(@RequestParam Integer id,@RequestParam String month){
+        List resultList= appQualityService.queryQualityDelay(id,month);
         ResponseMessage responseMessage = this.genResponseMessage();
         responseMessage.set("delay",resultList);
         return responseMessage;
@@ -76,14 +76,14 @@ public class AppQualityController extends ZRestController {
 
     /**
      *    查询功耗指标
-     * @param app
+     * @param id
      * @param month
      * @return
      *  @author limin
      */
     @RequestMapping( value = "queryQualityConsume", method = RequestMethod.GET)
-    public ResponseMessage queryQualityConsume(@RequestParam Integer app,@RequestParam String month){
-        List resultList= appQualityService.queryQualityConsume(app,month);
+    public ResponseMessage queryQualityConsume(@RequestParam Integer id,@RequestParam String month){
+        List resultList= appQualityService.queryQualityConsume(id,month);
         ResponseMessage responseMessage = this.genResponseMessage();
         responseMessage.set("consume",resultList);
         return responseMessage;
@@ -91,14 +91,14 @@ public class AppQualityController extends ZRestController {
 
     /**
      *    查询使用体验
-     * @param app
+     * @param id
      * @param month
      * @return
      *  @author limin
      */
     @RequestMapping( value = "queryQualityExperience", method = RequestMethod.GET)
-    public ResponseMessage queryQualityExperience(@RequestParam Integer app,@RequestParam String month){
-        List resultList= appQualityService.queryQualityExperience(app,month);
+    public ResponseMessage queryQualityExperience(@RequestParam Integer id,@RequestParam String month){
+        List resultList= appQualityService.queryQualityExperience(id,month);
         ResponseMessage responseMessage = this.genResponseMessage();
         responseMessage.set("experience",resultList);
         return responseMessage;
