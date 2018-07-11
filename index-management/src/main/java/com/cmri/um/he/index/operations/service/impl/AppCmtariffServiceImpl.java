fecc9ef59873c8b125a0565aa6af46b66c42c780
lim <2535780803@qq.com>
2018-07-11 15:50:39 +0800

    新增渠道、营销、资费、服务原始数据的接口

--- index-management/src/main/java/com/cmri/um/he/index/operations/service/impl/AppCmtariffServiceImpl.java
+++ index-management/src/main/java/com/cmri/um/he/index/operations/service/impl/AppCmtariffServiceImpl.java
@@ -1,9 +1,14 @@
 package com.cmri.um.he.index.operations.service.impl;
 
 import com.cmri.um.he.index.operations.dao.AppCmtariffDao;
+import com.cmri.um.he.index.operations.entity.AppOriginalContentEntity;
 import com.cmri.um.he.index.operations.service.AppCmtariffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * 	渠道、营销、资费、服务原始数据
  *
@@ -15,4 +20,20 @@ public class AppCmtariffServiceImpl implements AppCmtariffService {
 
     @Autowired
     private AppCmtariffDao appCmtariffDao;
+
+    @Override
+    public boolean saveAppOriginalContentEntity(List<AppOriginalContentEntity> list) {
+        Date date=new Date();
+        for (AppOriginalContentEntity entity :list) {
+            entity.setAtime(date);
+            appCmtariffDao.saveAppOriginalContentEntity(entity);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean dealAppOriginalContentEntity(List<AppOriginalContentEntity> list) {
+
+        return false;
+    }
 }
