478b543ae3bf0dfda88a71533ee8664e57a8d9e0
machao <17600309619@163.com>
2018-07-27 11:14:28 +0800

    市场指数二级页面应用概况统计

---
+++ index-server/src/main/java/com/cmri/um/he/index/common/Constants.java
@@ -0,0 +1,13 @@
+package com.cmri.um.he.index.common;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 数字常量通用类
+ * @author machao
+ * Created on 2018/7/27
+ */
+public class Constants {
+
+    public static int default_month = 12;
+}
