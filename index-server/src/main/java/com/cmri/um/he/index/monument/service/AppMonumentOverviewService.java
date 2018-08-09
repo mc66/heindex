325239a460c4ec9f3a03349807118e15f6da8d3e
machao <17600309619@163.com>
2018-08-09 15:31:07 +0800

    口碑页面建包

---
+++ index-server/src/main/java/com/cmri/um/he/index/monument/service/AppMonumentOverviewService.java
@@ -0,0 +1,8 @@
+package com.cmri.um.he.index.monument.service;
+
+import java.util.List;
+import java.util.Map;
+
+
+public interface AppMonumentOverviewService {
+}
---
+++ index-server/src/main/java/com/cmri/um/he/index/monument/service/impl/AppMonumentOverviewServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cmri.um.he.index.monument.service.impl;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AppMonumentOverviewServiceImpl implements AppMonumentOverviewServiceImpl {
+
+
+}
+
