6ae6390238da8acde71d073144e31ef7b99d871b
lim <2535780803@qq.com>
2018-08-16 09:50:43 +0800

    解决跨越

---
+++ index-server/src/main/java/com/cmri/um/he/index/common/VerifyCodeUtils.java
@@ -0,0 +1,69 @@
+package com.cmri.um.he.index.common;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * @author lch
+ *         Created on 2018/08/15 15:03
+ */
+public class VerifyCodeUtils {
+    private static StringBuffer codesave = null;
+
+    public static BufferedImage paintImage(int width, int height, int len) {
+        BufferedImage img = new BufferedImage(width, height, 1);
+
+        Graphics graphics = img.getGraphics();
+
+        graphics.setColor(Color.BLACK);
+
+        graphics.fillRect(0, 0, width, height);
+
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(1, 1, width - 2, height - 2);
+
+        String code = "23456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+        Random rd = new Random();
+        codesave = new StringBuffer();
+
+        graphics.setFont(new Font("楷体", 1, height - 4));
+        for (int i = 0; i < len; i++) {
+            graphics.setColor(new Color(rd.nextInt(256), rd.nextInt(256), rd.nextInt(256)));
+
+            int index = rd.nextInt(code.length());
+            codesave.append(code.substring(index, index + 1));
+
+            graphics.drawString(code.substring(index, index + 1), width / 8 * (i + 1), height - 4);
+        }
+
+        return img;
+    }
+
+    public static String getCode() {
+        if (codesave == null) {
+            return "";
+        }
+        return codesave.toString();
+    }
+
+    public static void generateCode(HttpServletRequest request, HttpServletResponse response, int width, int len)
+            throws ServletException, IOException {
+        BufferedImage img = paintImage(width, 30, len);
+        String code = VerifyCodeUtils.getCode();
+
+        HttpSession session = request.getSession();
+        session.removeAttribute("code");
+        session.setAttribute("code", code.toLowerCase());
+        System.out.println(session.getAttribute("code"));
+
+        ImageIO.write(img, "jpeg", response.getOutputStream());
+    }
+}
+
--- index-server/src/main/java/com/cmri/um/he/index/login/service/impl/AppLoginServiceImpl.java
+++ index-server/src/main/java/com/cmri/um/he/index/login/service/impl/AppLoginServiceImpl.java
@@ -9,8 +9,9 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.Max;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * 登陆页面
@@ -23,15 +24,40 @@ public class AppLoginServiceImpl implements AppLoginService {
     @Autowired
     private AppLoginDao appLoginDao;
 
+    /**
+     *  登陆页面
+     * @param username
+     * @param userpass
+     * @param request
+     * @param response
+     * @return
+     */
     @Override
-    public String getlogin(String username, String userpass, HttpServletRequest response) {
+    public String getlogin(String username, String userpass, HttpServletRequest request,HttpServletResponse response) {
 
        List<AppUserEntity> list = appLoginDao.queryByName(username);
 
         if (list.size()==1){
             List<AppUserEntity> list1 = appLoginDao.getlogin(username,userpass);
             if(list1.size()==1){
-                response.getSession().setAttribute("loginuser",list1.get(0));
+                request.getSession().setAttribute("id",list1.get(0).getId());
+                try {
+                    response.setHeader("Access-Control-Allow-Credentials","true");
+                    response.setHeader("Access-Control-Allow-Origin","http://localhost:9280");
+                    response.setHeader("Set-Cookie","token=cowshield");
+                    //将验证信息保存到Cookie
+                    Cookie cid=new Cookie("userid", UUID.randomUUID().toString().replaceAll("-", ""));
+                    Cookie cpass=new Cookie("password",userpass);
+                    cid.setPath("/");
+                    cpass.setPath("/");
+                    cid.setHttpOnly(true);
+                    cpass.setHttpOnly(true);
+                    response.addCookie(cid);
+                    response.addCookie(cpass);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 return "loginYes";
             }
             return "loginNo";
