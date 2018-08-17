1187e6e900524b8287a6ebf8fc3469370a4bfbda
shgit <shihao257@163.com>
2018-08-17 14:04:51 +0800

    添加注释

--- index-server/src/main/java/com/cmri/um/he/index/common/ValidateCode.java
+++ index-server/src/main/java/com/cmri/um/he/index/common/ValidateCode.java
@@ -1,148 +1,89 @@
 package com.cmri.um.he.index.common;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Random;
 
 public class ValidateCode {
-    // 图片的宽度。
-    private int width = 160;
-    // 图片的高度。
-    private int height = 40;
-    // 验证码字符个数
-    private int codeCount = 5;
-    // 验证码干扰线数
-    private int lineCount = 150;
-    // 验证码
-    private String code = null;
-    // 验证码图片Buffer
-    private BufferedImage buffImg = null;
+    private static String s = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+    private static int width = 60;
+    private static int height = 22;
+    private static int len = 30;
 
-    // 验证码范围,去掉0(数字)和O(拼音)容易混淆的(小写的1和L也可以去掉,大写不用了)
-    private char[] codeSequence = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-            'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public static void getVerifyCode(HttpServletRequest request,
+                                     HttpServletResponse response) {
+        //设置响应头无缓存
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
 
-    /**
-     * 默认构造函数,设置默认参数
-     */
-    public ValidateCode() {
-        this.createCode();
-    }
-
-    /**
-     * @param width  图片宽
-     * @param height 图片高
-     */
-    public ValidateCode(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.createCode();
-    }
-
-    /**
-     * @param width     图片宽
-     * @param height    图片高
-     * @param codeCount 字符个数
-     * @param lineCount 干扰线条数
-     */
-    public ValidateCode(int width, int height, int codeCount, int lineCount) {
-        this.width = width;
-        this.height = height;
-        this.codeCount = codeCount;
-        this.lineCount = lineCount;
-        this.createCode();
-    }
-
-    public void createCode() {
-        int x = 0, fontHeight = 0, codeY = 0;
-        int red = 0, green = 0, blue = 0;
-
-        x = width / (codeCount + 2);//每个字符的宽度(左右各空出一个字符)
-        fontHeight = height - 2;//字体的高度
-        codeY = height - 4;
-
-        // 图像buffer
-        buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = buffImg.createGraphics();
-        // 生成随机数
+        //创建一个图像缓冲区
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        //创建一个画笔
+        Graphics g = image.getGraphics();
         Random random = new Random();
-        // 将图像填充为白色
-        g.setColor(Color.WHITE);
+        //设置画笔随机颜色
+        g.setColor(getRandColor(220, 250));
+        //画矩形
         g.fillRect(0, 0, width, height);
-        // 创建字体,可以修改为其它的
-        Font font = new Font("Fixedsys", Font.PLAIN, fontHeight);
-//        Font font = new Font("Times New Roman", Font.ROMAN_BASELINE, fontHeight);
-        g.setFont(font);
-
-        for (int i = 0; i < lineCount; i++) {
-            // 设置随机开始和结束坐标
-            int xs = random.nextInt(width);//x坐标开始
-            int ys = random.nextInt(height);//y坐标开始
-            int xe = xs + random.nextInt(width / 8);//x坐标结束
-            int ye = ys + random.nextInt(height / 8);//y坐标结束
-
-            // 产生随机的颜色值，让输出的每个干扰线的颜色值都将不同。
-            red = random.nextInt(255);
-            green = random.nextInt(255);
-            blue = random.nextInt(255);
-            g.setColor(new Color(red, green, blue));
-            g.drawLine(xs, ys, xe, ye);
+        //设置字体
+        g.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        g.draw3DRect(0, 0, width - 2, height - 2, true);
+        //设置画线的随机颜色
+        g.setColor(getRandColor(160, 200));
+        //随机画30条线段
+        for (int i = 0; i < len; i++) {
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            int x1 = random.nextInt(12);
+            int y1 = random.nextInt(12);
+            g.drawLine(x, y, x + x1, y + y1);
+        }
+        //要显示的验证码
+        String sRand = "";
+        //验证码为4个随机字符
+        for (int i = 0; i < 4; i++) {
+            char rand = s.charAt(random.nextInt(s.length()));
+            sRand += rand;
+            //设置画随机字符的颜色
+            g.setColor(new Color(20 + random.nextInt(110), random.nextInt(110),
+                    random.nextInt(110)));
+            //验证码的范围在s中
+            g.drawString(String.valueOf(rand), 13 * i + 6, 16);
         }
 
-        // randomCode记录随机产生的验证码
-        StringBuffer randomCode = new StringBuffer();
-        // 随机产生codeCount个字符的验证码。
-        for (int i = 0; i < codeCount; i++) {
-            String strRand = String.valueOf(codeSequence[random.nextInt(codeSequence.length)]);
-            // 产生随机的颜色值，让输出的每个字符的颜色值都将不同。
-            red = random.nextInt(255);
-            green = random.nextInt(255);
-            blue = random.nextInt(255);
-            g.setColor(new Color(red, green, blue));
-            g.drawString(strRand, (i + 1) * x, codeY);
-            // 将产生的四个随机数组合在一起。
-            randomCode.append(strRand);
-        }
-        // 将四位数字的验证码保存到Session中。
-        code = randomCode.toString();
-    }
+        request.getSession().setAttribute("VerifyCode", sRand);
+        g.dispose();
 
-    public void write(String path) throws IOException {
-        OutputStream sos = new FileOutputStream(path);
-        this.write(sos);
-    }
-
-    public void write(OutputStream sos) throws IOException {
-        ImageIO.write(buffImg, "png", sos);
-        sos.close();
-    }
-
-    public BufferedImage getBuffImg() {
-        return buffImg;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    /**
-     * 测试函数,默认生成到d盘
-     * @param args
-     */
-    public static void main(String[] args) {
-        ValidateCode vCode = new ValidateCode(160,40,5,150);
+        ServletOutputStream output;
         try {
-            String path="D:/"+System.currentTimeMillis()+".png";
-            System.out.println(vCode.getCode()+" >"+path);
-            vCode.write(path);
+            //输出显示到浏览器中
+            output = response.getOutputStream();
+            ImageIO.write(image, "JPEG", output);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    //获取随机颜色rgb的值
+    private static Color getRandColor(int fc, int bc) {
+        Random random = new Random();
+        if (fc > 255)
+            fc = 255;
+        if (bc > 255)
+            bc = 255;
+        int r = fc + random.nextInt(bc - fc);
+        int g = fc + random.nextInt(bc - fc);
+        int b = fc + random.nextInt(bc - fc);
+        return new Color(r, g, b);
+    }
+
+
 
 }
--- index-server/src/main/java/com/cmri/um/he/index/login/controller/AppLoginController.java
+++ index-server/src/main/java/com/cmri/um/he/index/login/controller/AppLoginController.java
@@ -2,14 +2,16 @@ package com.cmri.um.he.index.login.controller;
 
 import com.cmri.spring.common.controller.ZRestController;
 import com.cmri.spring.common.data.ResponseMessage;
+import com.cmri.um.he.index.common.ValidateCode;
 import com.cmri.um.he.index.login.service.AppLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLDecoder;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,15 +20,20 @@ import java.util.Map;
  * Created on 2018/8/13
  */
 @RestController
-@CrossOrigin
 public class AppLoginController extends ZRestController {
 
     @Autowired
     private AppLoginService appLoginService;
 
     @RequestMapping(value = "login",method = RequestMethod.POST)
-    public Object getlogin(@RequestParam String username, @RequestParam String userpass, HttpServletRequest request){
+    public Object getlogin(@RequestParam String username, @RequestParam String userpass, HttpServletRequest request, HttpServletResponse response){
         String  login = appLoginService.getlogin(username,userpass,request);
+
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Origin","http://223.105.1.80");
+        response.setHeader("Set-Cookie","token=cowshield");
+        Cookie cookie =new Cookie("test",login);
+        response.addCookie(cookie);
         Map map = new HashMap(16);
        if (login.equals("loginYes")){
            map.put("code",200);
@@ -41,5 +48,27 @@ public class AppLoginController extends ZRestController {
        }
         return  map;
     }
+    @RequestMapping(value = "code",method = RequestMethod.GET)
+     public void getCode(HttpServletRequest request, HttpServletResponse response){
 
+        ValidateCode.getVerifyCode(request,response);
+    };
+
+    @RequestMapping(value = "app-test",method = RequestMethod.POST)
+    public ResponseMessage getTest(@RequestParam String username, @RequestParam String password,HttpServletRequest request, HttpServletResponse response)throws Exception{
+
+        username=URLDecoder.decode(username,"UTF-8");
+        password=URLDecoder.decode(password,"UTF-8");
+        Cookie cookie1 =new Cookie("username",username);
+        Cookie cookie2 =new Cookie("password",password);
+        response.addCookie(cookie1);
+        response.addCookie(cookie2);
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+       // response.setHeader("Access-Control-Allow-Origin: http://www.xxx.com");
+        ResponseMessage requestMessage =this.genResponseMessage();
+        requestMessage.setMsg("成功");
+        return  requestMessage;
+
+    };
 }
