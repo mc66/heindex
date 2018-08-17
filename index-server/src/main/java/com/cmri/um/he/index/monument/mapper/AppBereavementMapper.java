f9b00d049aab486ef18b6f1dcad42b3083ae1ca3
yjj <yjj@zpark.com.cn>
2018-08-17 15:55:18 +0800

    修改评论数量统计2

--- index-server/src/main/java/com/cmri/um/he/index/monument/mapper/AppBereavementMapper.java
+++ index-server/src/main/java/com/cmri/um/he/index/monument/mapper/AppBereavementMapper.java
@@ -70,8 +70,10 @@ public interface AppBereavementMapper {
      * @param endTime 结束时间
      * @return 结果集
      * */
-    @Select("SELECT freq_positive,freq_negativity,freq_neutral,freq_sum\n" +
-            "FROM app_emotion_parameter WHERE app =#{app} AND `month` BETWEEN #{startTime} AND #{endTime}")
+    @Select("SELECT word_name name,SUM(word_frequency) value\n" +
+            "FROM hot_word WHERE app =#{app}\n" +
+            " AND date BETWEEN #{startTime} AND #{endTime}\n" +
+            "GROUP BY word_name")
     List<Map<String,Object>> frequencyCount(@Param("app") Integer app,@Param("startTime") String startTime,@Param("endTime") String endTime);
 
 
@@ -82,7 +84,7 @@ public interface AppBereavementMapper {
      * @param endTime 结束时间
      * @return 结果集
      * */
-    @Select("SELECT app, SUM(freq_positive) freq_positive,SUM(freq_negativity) freq_negativity,SUM(freq_neutral) freq_neutral,SUM(freq_sum) freq_sum\n" +
+    @Select("SELECT freq_positive,freq_negativity,freq_neutral,freq_sum\n" +
             "FROM app_emotion_parameter WHERE app = #{app} AND `month` BETWEEN #{startTime} AND #{endTime}")
     List<Map<String,Object>> findPositive(@Param("app") Integer app,@Param("startTime") String startTime,@Param("endTime") String endTime);
 
