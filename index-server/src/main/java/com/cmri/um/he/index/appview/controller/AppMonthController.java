0476fb282b732d148384b77fa444e8d393eda6b4
lch <929664394@qq.com>
2018-08-23 15:16:33 +0800

    修改应用类别的筛选条件为有内容才展示

--- index-server/src/main/java/com/cmri/um/he/index/appview/controller/AppMonthController.java
+++ index-server/src/main/java/com/cmri/um/he/index/appview/controller/AppMonthController.java
@@ -4,7 +4,6 @@ import com.cmri.spring.common.controller.ZRestController;
 import com.cmri.spring.common.data.ResponseMessage;
 import com.cmri.um.he.index.appview.service.AppMonthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- index-server/src/main/java/com/cmri/um/he/index/appview/mapper/AppCategoryMapper.java
+++ index-server/src/main/java/com/cmri/um/he/index/appview/mapper/AppCategoryMapper.java
@@ -17,11 +17,11 @@ public interface AppCategoryMapper {
      * 查询应用类别
      * @return 应用类别集合
      */
-    @Select("SELECT id, name FROM app_category order by id")
+    @Select("SELECT id, name FROM app_category WHERE `status`=1 order by id")
     List<Map<String,Object>> findCategory();
 
     /**
-     *c查询应用类别及对应的app数据
+     * 查询应用类别及对应的app数据
      * @return 应用类别集合
      */
     @Select("SELECT  info.id app_id , category.`name` cate,category.id category_id,info.`name`,CONCAT(category.`name`,'报告') AS newcal\n" +
