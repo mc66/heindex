5aed1b4b8d4a4e7d6da2cdf090b573600750372c
lim <2535780803@qq.com>
2018-08-29 16:34:48 +0800

    缓存口碑

--- index-server/src/main/java/com/cmri/um/he/index/reputation/service/impl/AppBereavementServiceImpl.java
+++ index-server/src/main/java/com/cmri/um/he/index/reputation/service/impl/AppBereavementServiceImpl.java
@@ -3,6 +3,7 @@ package com.cmri.um.he.index.reputation.service.impl;
 import com.cmri.um.he.index.reputation.dao.AppBereavementDao;
 import com.cmri.um.he.index.reputation.service.AppBereavementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 情感指数变化趋势的服务类的服务类实现
@@ -22,83 +24,125 @@ public class AppBereavementServiceImpl implements AppBereavementService {
 
     @Autowired
     private AppBereavementDao dao;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public List<Map<String, Object>> findBereavement(Integer category, String startTime, String endTime) {
-
-        List<Map<String, Object>> bereavment = dao.findBereavement(category, startTime, endTime);
-
+        String key = category.toString()+startTime+endTime+"findBereavement";
+        Object kobei1 = redisTemplate.opsForValue().get(key);
+        List<Map<String, Object>> bereavment = null;
+        if (kobei1 == null){
+            bereavment = dao.findBereavement(category, startTime, endTime);
+            redisTemplate.opsForValue().set(key,bereavment,30, TimeUnit.DAYS);
+        }else {
+            bereavment = (List<Map<String, Object>>) redisTemplate.opsForValue().get(key);
+        }
         return bereavment;
     }
 
     @Override
     public List<Map<String, Object>> findMoonEmotion(Integer category, String endTime) {
-        List<Map<String, Object>> bereavment = dao.findMoonEmotion(category, endTime);
+        String key = category.toString()+endTime+"findMoonEmotion";
+        Object kobei1 = redisTemplate.opsForValue().get(key);
+        List<Map<String, Object>> bereavment = null;
+        if(kobei1 == null){
+            bereavment = dao.findMoonEmotion(category, endTime);
+            redisTemplate.opsForValue().set(key,bereavment,30, TimeUnit.DAYS);
+        }else {
+            bereavment = (List<Map<String, Object>>) redisTemplate.opsForValue().get(key);
+        }
+
         return bereavment;
 
     }
 
     @Override
     public List<Map<String, Object>> frequencyCount(Integer app, String startTime, String endTime) {
-        List<Map<String, Object>> mapList = dao.frequencyCount(app, startTime, endTime);
+        String key = app.toString()+startTime+endTime+"frequencyCount";
+        Object kobei1 = redisTemplate.opsForValue().get(key);
+        List<Map<String, Object>> mapList = null;
+        if (kobei1 == null){
+            mapList = dao.frequencyCount(app, startTime, endTime);
+            redisTemplate.opsForValue().set(key,mapList,30, TimeUnit.DAYS);
+        }else {
+            mapList = (List<Map<String, Object>>) redisTemplate.opsForValue().get(key);
+        }
         return mapList;
     }
 
     @Override
     public List<Map<String, Object>> findCategory(Integer category) {
-
-        return dao.findCategory(category);
+        String key = category.toString()+"findCategory";
+        Object kobei1 = redisTemplate.opsForValue().get(key);
+        List<Map<String, Object>> list = null;
+        if (kobei1 == null){
+            list = dao.findCategory(category);
+            redisTemplate.opsForValue().set(key,list,30, TimeUnit.DAYS);
+        }else {
+            list = (List<Map<String, Object>>) redisTemplate.opsForValue().get(key);
+        }
+        return list;
     }
+
 
     @Override
     public  List<Map<String, Object>> findParameter(Integer category, String startTime, String endTime) {
-        List<Map<String, Object>> appName = dao.findCategory(category);
+        String key = category.toString()+startTime+endTime+"findParameter";
+        Object kobei1 = redisTemplate.opsForValue().get(key);
         List<Map<String, Object>> appNames = new ArrayList<>();
-        for (Map<String, Object> map : appName) {
-            Map<String, Object> maps=new HashMap<>();
-            int app =(int) map.get("id");
-            String name =(String) map.get("name");
-            int freq_sum = dao.getFavorableRate(category, app, startTime, endTime);
-            if(freq_sum==0){
-                maps.put("name",name);
-                maps.put("freq_sum",0);
-                maps.put("freq_positive",0);
-                maps.put("freq_neutral",0);
-                maps.put("freq_negativity",0);
-                maps.put("pet_positives","0.0%");
-                maps.put("pet_negativitys","0.0%");
-                maps.put("pet_neutrals","0.0%");
-            }else {
-                int freq_positive = dao.getFavorableRate1(category, app, startTime, endTime);
-                int freq_neutral = dao.getFavorableRate2(category, app, startTime, endTime);
-                int freq_negativity = dao.getFavorableRate3(category, app, startTime, endTime);
-                double freq_sums = (double) freq_sum;
-                double pet_positive = new BigDecimal((float) freq_positive / freq_sums * 100).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-                String pet_positives = Double.toString(pet_positive);
-                pet_positives = pet_positives + "%";
-                double pet_negativity = new BigDecimal((float) freq_negativity / freq_sums * 100).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-                String pet_negativitys = Double.toString(pet_negativity);
-                pet_negativitys = pet_negativitys + "%";
-                double pet_neutral = new BigDecimal((float) freq_neutral / freq_sums * 100).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-                String pet_neutrals = Double.toString(pet_neutral);
-                pet_neutrals = pet_neutrals + "%";
+        if (kobei1 == null) {
+            List<Map<String, Object>> appName = dao.findCategory(category);
+            for (Map<String, Object> map : appName) {
+                Map<String, Object> maps = new HashMap<>();
+                int app = (int) map.get("id");
+                String name = (String) map.get("name");
+                int freq_sum = dao.getFavorableRate(category, app, startTime, endTime);
+                if (freq_sum == 0) {
+                    maps.put("name", name);
+                    maps.put("freq_sum", 0);
+                    maps.put("freq_positive", 0);
+                    maps.put("freq_neutral", 0);
+                    maps.put("freq_negativity", 0);
+                    maps.put("pet_positives", "0.0%");
+                    maps.put("pet_negativitys", "0.0%");
+                    maps.put("pet_neutrals", "0.0%");
+                } else {
+                    int freq_positive = dao.getFavorableRate1(category, app, startTime, endTime);
+                    int freq_neutral = dao.getFavorableRate2(category, app, startTime, endTime);
+                    int freq_negativity = dao.getFavorableRate3(category, app, startTime, endTime);
+                    double freq_sums = (double) freq_sum;
+                    double pet_positive = new BigDecimal((float) freq_positive / freq_sums * 100).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+                    String pet_positives = Double.toString(pet_positive);
+                    pet_positives = pet_positives + "%";
+                    double pet_negativity = new BigDecimal((float) freq_negativity / freq_sums * 100).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+                    String pet_negativitys = Double.toString(pet_negativity);
+                    pet_negativitys = pet_negativitys + "%";
+                    double pet_neutral = new BigDecimal((float) freq_neutral / freq_sums * 100).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+                    String pet_neutrals = Double.toString(pet_neutral);
+                    pet_neutrals = pet_neutrals + "%";
 
-                maps.put("name",name);
-                maps.put("freq_sum",freq_sum);
-                maps.put("freq_positive",freq_positive);
-                maps.put("freq_neutral",freq_neutral);
-                maps.put("freq_negativity",freq_negativity);
-                maps.put("pet_positives",pet_positives);
-                maps.put("pet_negativitys",pet_negativitys);
-                maps.put("pet_neutrals",pet_neutrals);
+                    maps.put("name", name);
+                    maps.put("freq_sum", freq_sum);
+                    maps.put("freq_positive", freq_positive);
+                    maps.put("freq_neutral", freq_neutral);
+                    maps.put("freq_negativity", freq_negativity);
+                    maps.put("pet_positives", pet_positives);
+                    maps.put("pet_negativitys", pet_negativitys);
+                    maps.put("pet_neutrals", pet_neutrals);
+
+                }
+                appNames.add(maps);
+                redisTemplate.opsForValue().set(key,appNames,30, TimeUnit.DAYS);
 
             }
-            appNames.add(maps);
-            }
+        } else {
+            appNames = (List<Map<String, Object>>) redisTemplate.opsForValue().get(key);
+        }
          return appNames;
-
-
-
     }
+
+
+
 
 }
