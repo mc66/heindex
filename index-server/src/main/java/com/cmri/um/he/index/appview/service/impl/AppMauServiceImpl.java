9c6737295d6843a0d1de5dec738feb88f348b20a
machao <17600309619@163.com>
2018-08-29 17:17:33 +0800

    表字段修改

--- index-server/src/main/java/com/cmri/um/he/index/appview/service/impl/AppMauServiceImpl.java
+++ index-server/src/main/java/com/cmri/um/he/index/appview/service/impl/AppMauServiceImpl.java
@@ -6,6 +6,7 @@ import com.cmri.um.he.index.appview.service.AppActiveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,20 +22,35 @@ public class AppMauServiceImpl implements AppActiveService {
 
     @Override
     public PagingData<Map<String, Object>> find(String month, int page, int step) {
-        return new PagingData<>(activeDao.count(month),
+        PagingData<Map<String, Object>> pagingData = new PagingData<>(activeDao.count(month),
                 page,
                 step,
                 activeDao.find(month, page, step)
         );
+        return getPagingData(pagingData);
     }
 
     @Override
     public PagingData<Map<String, Object>> find(String month, String category, int page, int step) {
-        return new PagingData<>(activeDao.countOfCategory(month, category),
+        PagingData<Map<String, Object>> pagingData = new PagingData<>(activeDao.countOfCategory(month, category),
                 page,
                 step,
                 activeDao.findByCategory(month, category, page, step)
         );
+        return getPagingData(pagingData);
+
+    }
+    public static PagingData<Map<String, Object>> getPagingData(PagingData<Map<String, Object>> pagingData){
+        List<Map<String, Object>> data = pagingData.getData();
+        for (Map<String, Object> datum : data) {
+            double datum1 = (double)datum.get("datum");
+            if (datum1>0){
+                datum.put("status",1);
+            }else {
+                datum.put("status",0);
+            }
+        }
+        return pagingData;
     }
 
 }
