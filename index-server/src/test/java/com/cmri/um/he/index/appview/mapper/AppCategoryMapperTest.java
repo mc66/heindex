8d0cbd76b235da9e3f970ba6e5af58bc2186c630
machao <17600309619@163.com>
2018-09-13 10:45:10 +0800

    修改排序方式

--- index-server/src/test/java/com/cmri/um/he/index/appview/mapper/AppCategoryMapperTest.java
+++ index-server/src/test/java/com/cmri/um/he/index/appview/mapper/AppCategoryMapperTest.java
@@ -1,5 +1,8 @@
 package com.cmri.um.he.index.appview.mapper;
 
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,5 +29,31 @@ public class AppCategoryMapperTest {
     public void countOfCategory() {
         List<Map<String, Object>> categorys = mapper.findCategorys();
         Assert.assertTrue(categorys.size() > 0);
+    }
+
+    @Autowired
+    JestClient jestClient;
+
+    @Test
+    public void testEs() throws IOException {
+        String query="{\n" +
+                "  \"query\": {\n" +
+                "    \"match\": {\n" +
+                "      \"actorList.name\": \"张译\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        Search search = new Search.Builder(query).addIndex("movie_chn").addType("movie").build();
+
+        SearchResult result = jestClient.execute(search);
+
+        List<SearchResult.Hit<HashMap, Void>> hits = result.getHits(HashMap.class);
+
+        for (SearchResult.Hit<HashMap, Void> hit : hits) {
+            HashMap source = hit.source;
+            System.err.println("source = " + source);
+
+        }
+
     }
 }
